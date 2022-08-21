2562552ed8d06378d61b2a527c419afb3f17378a
Rodion D <rodiond26@gmail.com>
2022-08-21 21:24:14 +0500

    add tests

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -15,10 +15,10 @@ func init() {
 	flag.StringVar(&to, "to", "", "file to write to")
 	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
 	flag.Int64Var(&offset, "offset", 0, "offset in input file")
-	flag.Parse()
 }
 
 func main() {
+	flag.Parse()
 	err := Copy(from, to, offset, limit)
 	if err != nil {
 		fmt.Printf("script error [%s]\n", err)
