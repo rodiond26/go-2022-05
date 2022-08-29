0e9d55431334ec3b61d219d53ba4c4f0b6266d4c
Rodion D <rodiond26@gmail.com>
2022-08-29 19:02:43 +0500

    start resolving hw9

--- hw09_struct_validator/go.mod
+++ hw09_struct_validator/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw09_struct_validator
+module github.com/rodiond26/go-2022-05/hw09_struct_validator
 
 go 1.16
--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,10 @@
 package hw09structvalidator
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -7,11 +12,49 @@ type ValidationError struct {
 
 type ValidationErrors []ValidationError
 
+const (
+	validateTag = "validate"
+)
+
 func (v ValidationErrors) Error() string {
 	panic("implement me")
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
-	return nil
+	s := reflect.ValueOf(v)
+	if s.IsNil() {
+		return nil // TODO fix
+	}
+	if s.Kind() != reflect.Struct {
+		return nil // TODO fix
+	}
+
+	vType := s.Type()
+	for i := 0; i < vType.NumField(); i++ {
+		field := vType.Field(i)
+		tag := field.Tag.Get(validateTag)
+		if tag == "" {
+			// TODO ?
+			continue
+		}
+		err := validateField(field.Name, validateTag, vType.Field(i))
+		fmt.Println(err) // TODO delete
+	}
+
+	return nil // TODO fix
+}
+
+func validateField(fieldName, validateTag string, field reflect.StructField) error {
+	return nil // TODO fix
+
+}
+
+type App1 struct {
+	Version string `validate:"len:5"`
+}
+
+func main() {
+	app1 := App1{Version: "123"}
+	err := Validate(app1)
+	fmt.Println(err)
 }
